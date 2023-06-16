2d675678db94b0e1be9bf4237a5a149b1ff2e5bf
Ciel-MC <47144701+Ciel-MC@users.noreply.github.com>
2023-06-17 00:32:43 +0800

    refactor: Move from Java to Kotlin

---
+++ src/main/kotlin/com/github/shuaiouke/redlibcommandfile/language/RedLibCommandLanguage.kt
@@ -0,0 +1,5 @@
+package com.github.shuaiouke.redlibcommandfile.language
+
+import com.intellij.lang.Language
+
+object RedLibCommandLanguage: Language("RLCF")
---
+++ src/main/kotlin/com/github/shuaiouke/redlibcommandfile/language/RedLibCommandLexerAdapter.kt
@@ -0,0 +1,5 @@
+package com.github.shuaiouke.redlibcommandfile.language
+
+import com.intellij.lexer.FlexAdapter
+
+class RedLibCommandLexerAdapter: FlexAdapter(RedLibCommandLexer(null))
